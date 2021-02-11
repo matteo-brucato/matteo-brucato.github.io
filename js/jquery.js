6eba6b80de86abe656fa47ee433120c388ea2bb3
Matteo <mattfeel@gmail.com>
2021-02-11 19:32:34 +0100

    Updates.

--- js/jquery.js
+++ js/jquery.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    var slide_toggle = $(".slide-toggle");
-    slide_toggle.mouseover(function() {
-        $(this).css("cursor", "pointer");
-    });
-    slide_toggle.click(function () {
-        $(this).next().slideToggle();
-    });
+    // var slide_toggle = $(".slide-toggle");
+    // slide_toggle.mouseover(function() {
+    //     $(this).css("cursor", "pointer");
+    // });
+    // slide_toggle.click(function () {
+    //     $(this).next().slideToggle();
+    // });
 
     var pub_list = $("#publication-list");
     var n_items = pub_list.find("ol li").size();
